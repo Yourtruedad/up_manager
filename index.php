84997bf61bcd796fc85cc27be371cb28553765bd
Yourtruedad <dev@pc38.home>
2015-11-03 22:40:22 +0100

    add composer monolog package

--- index.php
+++ index.php
@@ -3,3 +3,15 @@
 require __DIR__ . '/vendor/autoload.php';
 
 echo 'UPMANAGER Test';
+
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('app.log', Logger::WARNING));
+
+// add records to the log
+$log->addWarning('Foo');
+$log->addError('Bar');
