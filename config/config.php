466ce672daa28ad56c40f177e60d486b62a16d5f
Yourtruedad <dev@PPrzeworowski-MBPro.local>
2015-11-08 19:41:34 +0100

    config details

--- config/config.php
+++ config/config.php
@@ -1,3 +1,10 @@
 <?php
 
-$test = 'config';
+define('CONFIG_APP_NAME', 'UPMANAGER');
+
+define('CONFIG_DATABASE_HOST', 'localhost');
+define('CONFIG_DATABASE_NAME', 'test');
+define('CONFIG_DATABASE_USER', 'root');
+define('CONFIG_DATABASE_PASSWORD', '');
+
+define('CONFIG_LOG_DIR', 'app.log');
